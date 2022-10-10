fc72aa2bc95fe3e7ef07032d57fe6df225bddbe8
Em1X <113462360+Em1X-UA@users.noreply.github.com>
2022-10-10 10:10:12 +0300

    Update task6.py
    HT_02 Task6 update

--- HT_02/task6.py
+++ HT_02/task6.py
@@ -1,14 +1,13 @@
 """6. Write a script to check whether a value from user input is contained in 
 a group of values."""
 
+
 def check_val_in_list(list_, val):
-	for i in list_:
-		if val == str(i):
-			return True
-	return False
+    for i in list_:
+        if val == str(i):
+            return True
+    return False
 
 
 values_list = [1, 2, 'u', 'a', 4, True]
-input_ = input('Enter value to check: ')
-
-print(check_val_in_list(values_list, input_))
+print(check_val_in_list(values_list, input('Enter value to check: ')))
