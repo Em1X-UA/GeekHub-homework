85612e7989ebd1d83a5a5418412e4b8f310a821f
Em1X <113462360+Em1X-UA@users.noreply.github.com>
2022-10-10 10:37:43 +0300

    Update HT_2 Task1 and Task6

--- HT_02/task1.py
+++ HT_02/task1.py
@@ -1,26 +1,8 @@
 """1. Write a script which accepts a sequence of comma-separated numbers from
 user and generates a list and a tuple with those numbers."""
 
-#user_input = '1, 3, 5, 7, 9, 150, 5.0, asd'
-user_input = input('Enter numbers: ')
 
-my_list = []
-
-for el in user_input.split(','):
-	el = el.strip()
-	
-	try:
-		val = int(el)
-		my_list.append(val)
-
-	except ValueError:
-		try:
-			val = float(el)
-			my_list.append(val)
-		except ValueError:
-			print(f'Input "{el}" is not a number')
-
-
+my_list = input('Enter numbers: ').split(',')
 my_tuple = tuple(my_list)
 
 # print(my_list)
--- HT_02/task6.py
+++ HT_02/task6.py
@@ -2,12 +2,5 @@
 a group of values."""
 
 
-def check_val_in_list(list_, val):
-    for i in list_:
-        if val == str(i):
-            return True
-    return False
-
-
-values_list = [1, 2, 'u', 'a', 4, True]
-print(check_val_in_list(values_list, input('Enter value to check: ')))
+values_list = ['1', '2', 'u', 'a', '4', 'True']
+print(input('Enter value to check: ') in values_list)
