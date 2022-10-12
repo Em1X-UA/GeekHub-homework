27a03598b85ef6e83c841f5a470eb5c3bab9d037
Em1X <113462360+Em1X-UA@users.noreply.github.com>
2022-10-12 23:25:03 +0300

    HT_03 Task1 edited

--- HT_03/task1.py
+++ HT_03/task1.py
@@ -10,10 +10,14 @@ my_list = []
 for _ in range(randint(2, 7)):
 	my_list.append(tuple(randint(0, 100) for __ in range(randint(3, 6))))
 
+# my_list = [(1, 2, 3), (), ('asd', 4, True)]
 # print(my_list)
 # user_input = '-5'
 
 user_input = input('Enter replacement for last tuple elements: ')
-my_list = [tpl[:-1] + (user_input,) for tpl in my_list]
+
+for i in range(len(my_list)):
+	if my_list[i]:
+		my_list[i] = my_list[i][:-1] + (user_input,)
 
 print(my_list)
