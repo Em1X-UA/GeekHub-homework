1930b526275e0a5685ee51afaacccd410394ce85
Em1X <113462360+Em1X-UA@users.noreply.github.com>
2022-10-19 13:53:43 +0300

    Update HT_05 task7.py v2

--- HT_05/task7.py
+++ HT_05/task7.py
@@ -8,14 +8,16 @@
 
 def count_same(*args):
     count_dict = {}
+    result = []
     for el in args:
         if str(el) not in count_dict.keys():
             count_dict.update({str(el): 1})
         else:
             count_dict.update({str(el): count_dict.get(str(el)) + 1})
     for k, v in count_dict.items():
-        print(f'Value "{k}" passed in function {v} times.')
-    return
+        # print(f'Value "{k}" passed in function {v} times.')
+        result.append(f'{k} -> {v}')
+    return ', '.join(result)
 
 
-count_same(1, 1, 'foo', [1, 2], True, 'foo', 1, [1, 2])
+print(count_same(1, 1, 'foo', [1, 2], True, 'foo', 1, [1, 2]))
