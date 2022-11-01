f1cc8b09670fe666073d493c6c3c58a1df32ed3c
Em1X <113462360+Em1X-UA@users.noreply.github.com>
2022-11-01 11:33:40 +0200

    Update task2.py

--- HT_08/task2/task2.py
+++ HT_08/task2/task2.py
@@ -31,28 +31,19 @@ def read_file(filename, chars: int) -> list:
     else:
         with open(filename) as file:
             content = file.read()
-            mid_string = int(len(content) / 2)
+        mid_string = int(len(content) / 2)
 
-            if chars > len(content):
-                print(f'Attention! File {filename} length is less than '
-                      f'input chars qty: {chars}! Returned listed file text')
-                result = [content]
-            elif chars >= mid_string:
-                print(f'Attention! File {filename} doesn\'t have enough symbols to split '
-                      f'for 3 parts with input chars q-ty: {chars}! Returned 2')
-                result = [content[:chars], content[chars:]]
-            elif chars * 3 >= len(content):
-                print(f'Attention! File {filename} doesn\'t have enough '
-                      f'symbols to split for 3 equal parts with input '
-                      f'chars {chars} q-ty! Returned 3, but last is less')
-                result = [content[:chars], content[chars:chars * 2],
-                          content[chars * 2:chars * 3]]
-            else:
-                first = content[:chars]
-                middle = content[int(mid_string - chars / 2):int(mid_string + chars / 2)]
-                last = content[-chars::]
-                result = [text for text in (first, middle, last)]
-            return result
+        if chars > len(content):
+            print(f'Attention! File {filename} length ({len(content)}) '
+                  f'is less than input chars qty: {chars}!')
+            print(f'Returned 3 full-file (equals {filename}) strings')
+            result = [content] * 3
+        else:
+            first = content[:chars]
+            middle = content[int(mid_string - chars / 2):int(mid_string + chars / 2)]
+            last = content[-chars::]
+            result = [text for text in (first, middle, last)]
+        return result
 
 
 def main():
