808519a4eff452191e120fa4685c8712fb04a63f
Em1X <113462360+Em1X-UA@users.noreply.github.com>
2022-12-26 12:40:17 +0200

    HT_16 task1 visual fix

--- HT_16/task1/task1/spiders/chrome_webstore_spider.py
+++ HT_16/task1/task1/spiders/chrome_webstore_spider.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup as Bs
 from ..items import ExtensionItem
 
 
-
 class ChromeWebstoreSpider(scrapy.Spider):
     name = 'chrome_webstore_spider'
     allowed_domains = ['chrome.google.com']
@@ -20,7 +19,6 @@ class ChromeWebstoreSpider(scrapy.Spider):
     def start_requests(self):
         for url in self.start_urls:
             yield Request(url=url, headers=self.headers, callback=self.parse)
-            # return Request(url=url, headers=self.headers, callback=self.parse)
 
     def parse(self, response, **kwargs):
         """Parse URLs from first page"""
@@ -36,10 +34,8 @@ class ChromeWebstoreSpider(scrapy.Spider):
     def parse_item(response):
         """Parse single extension item"""
         page = Bs(response.body, 'lxml')
-        # print(dict(
         yield ExtensionItem(
             id=response.url.split('/')[-1].strip().split('?')[0],
             name=page.select_one('.e-f-w').text.strip(),
             description=page.select_one('.C-b-p-j-Pb').text.strip()
         )
-        # )
